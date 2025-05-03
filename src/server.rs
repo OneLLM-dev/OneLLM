b8950791877c758a486c6fa1c968f24893224831
TeenCoder159 <no1umang@gmail.com>
2025-05-04 00:04:36 +0800

    Add api handling

--- src/server.rs
+++ src/server.rs
@@ -1,31 +1,85 @@
 #[allow(unused)]
 use axum::{
-    Router,
-    extract::{Json, Query},
+    Json, Router,
+    extract::Query,
     routing::{get, post},
 };
 
-#[allow(unused)]
 use serde::{Deserialize, Serialize};
+use serde_json::{Value, json};
 
 use crate::database::User;
 use crate::requests::Input;
 
+#[derive(Deserialize, Serialize)]
+struct Output {
+    code: u32,
+    data: Value,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Data {
+    pub max_tokens: u128,
+}
+
 #[allow(unused)]
-async fn server() {
+pub async fn server() {
     let app = Router::new().route("/api", get(handle));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-#[allow(unused)]
-
-async fn handle(Query(apikey): Query<String>, Json(payload): Json<Input>) -> String {
-    let user = match User::get_row(apikey).await {
+pub async fn handle(Query(apikey): Query<String>, Json(payload): Json<Input>) -> String {
+    let user = match User::get_row(apikey.clone()).await {
         Ok(user_struct) => user_struct,
         Err(e) => return e.to_string(),
     };
 
-    String::new()
+    if user.apikey != apikey {
+        return unauthorised_apikey();
+    }
+
+    let _: Data = match serde_json::from_value::<Value>(payload.data.clone()) {
+        Ok(_) => return unauthorised_field_provided(),
+        Err(_) => Data {
+            max_tokens: user.balance as u128,
+        },
+    };
+
+    let output = match payload.get().await {
+        Ok(result) => result,
+        Err(_) => {
+            return json!({
+                "code": 500,
+                "output": "Error during payload processing"
+            })
+            .to_string();
+        }
+    };
+
+    // Return the successful response
+    json!({
+        "code": 200,
+        "output": output
+    })
+    .to_string()
+}
+
+/*------Unauthorised String returning functions------*/
+
+fn unauthorised_apikey() -> String {
+    let response = json!({
+        "code": 401,
+        "output": "Unauthorized"
+    });
+    return response.to_string();
+}
+
+fn unauthorised_field_provided() -> String {
+    let response = json!({
+        "code": 403,
+        "output": "You are not allowed to provide the field 'is_admin'"
+    });
+    return response.to_string();
 }
