0a82a495f299c63383da02af09f568669d5efd35
TeenCoder159 <no1umang@gmail.com>
2025-05-03 18:34:22 +0800

    Change main.rs for testing

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
-#![allow(unused)]
+// #![allow(unused)]
 
-use auth::{generate_api, signup};
+use auth::signup;
 use database::User;
 // use oneAI::Input;
 mod auth;
 mod database;
+mod requests;
+mod server;
 
-fn read_line() -> String {
-    let mut user_input = String::new();
-    std::io::stdin()
-        .read_line(&mut user_input)
-        .expect("Error reading line");
-    user_input.trim().to_string()
-}
+// fn read_line() -> String {
+//     let mut user_input = String::new();
+//     std::io::stdin()
+//         .read_line(&mut user_input)
+//         .expect("Error reading line");
+//     user_input.trim().to_string()
+// }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // db_testing().await?;
+    Ok(())
+}
+
+#[allow(unused)]
+async fn db_testing() -> Result<(), Box<dyn std::error::Error>> {
     match database::init_db().await {
         Ok(_) => {
             println!("Successfully created a new database")
@@ -29,18 +37,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     };
 
-    let user = signup(
+    let _user = signup(
         String::from("something@gmail.com"),
         String::from("Something__132;;"),
     )
     .unwrap();
-    println!("{:#?}", user);
 
-    // user.new_user().await.expect("Error");
+    let get = User::get_row("oa-94763257652657558374".to_string()).await?;
 
-    let something = user
-        .update_db(database::TableFields::Password, "somethingElse--_:;")
-        .await?;
+    println!("{get:#?}");
 
+    // let user_something = user.new_user().await.expect("Error");
+
+    // let _ = user
+    //     .update_db(database::TableFields::Password, "somethingElse--_:;")
+    //     .await?;
+    //
     Ok(())
 }
