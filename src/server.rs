f0cbf59503b6607b0138300553c14b6da7761cc0
TeenCoder159 <no1umang@gmail.com>
2025-06-09 21:35:03 +0800

    Add auth stuff

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 #[allow(unused)]
 use axum::{
     Json, Router,
@@ -12,7 +14,7 @@ use tower_http::services::ServeDir;
 
 use serde_json::{Value, json};
 
-use crate::auth;
+use crate::auth::{self, login, update_bal};
 use crate::requests::Input;
 use crate::utils::*;
 
@@ -20,9 +22,12 @@ use crate::utils::*;
 pub async fn server() {
     let app = Router::new()
         .fallback_service(ServeDir::new("frontend"))
-        .route("/api", get(handle_api));
+        .route("/api", get(handle_api))
+        .route("/post-backend", get(handle_post_website));
+    let ipaddr = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(ipaddr).await.unwrap();
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening at: {}", ipaddr);
     axum::serve(listener, app).await.unwrap();
 }
 
@@ -58,7 +63,7 @@ pub async fn handle_api(headers: HeaderMap, Json(payload): Json<Input>) -> Json<
             }),
         });
     };
-    let user = match User::get_row(apikey.clone()).await {
+    let user = match User::get_row_api(apikey.clone()).await {
         Ok(user_struct) => user_struct,
         Err(e) => {
             return Json(Output {
@@ -75,10 +80,10 @@ pub async fn handle_api(headers: HeaderMap, Json(payload): Json<Input>) -> Json<
     }
 
     let _: Data = match serde_json::from_value::<Value>(payload.data.clone()) {
-        Ok(_) => return unauthorised_field_provided(),
-        Err(_) => Data {
+        Ok(_) => Data {
             max_tokens: user.balance as u128,
         },
+        Err(_) => return unauthorised_field_provided(),
     };
 
     let output = match payload.get().await {
@@ -100,27 +105,90 @@ pub async fn handle_api(headers: HeaderMap, Json(payload): Json<Input>) -> Json<
     })
 }
 
-pub async fn _handle_website(Json(query): Json<WebInput>) -> Json<WebOutput> {
+pub async fn signup_and_update_db(
+    email: String,
+    password: String,
+) -> Result<Option<User>, Box<dyn std::error::Error>> {
+    let user = match auth::signup(email, password).await {
+        Some(a) => a,
+        None => return Ok(None),
+    };
+
+    match user.new_user().await {
+        Ok(()) => {}
+        Err(_) => return Ok(None),
+    };
+
+    Ok(None)
+}
+
+#[derive(Debug)]
+struct AuthError(String);
+impl Error for AuthError {}
+
+impl std::fmt::Display for AuthError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Update error: {}", self.0)
+    }
+}
+
+pub async fn handle_post_website(Json(query): Json<WebInput>) -> Json<FailOrSucc> {
     match query.function {
-        WebQuery::Login => {}
         WebQuery::Signup => {
-            let user = auth::signup(
-                query.data["email"].to_string(),
-                query.data["password"].to_string(),
-            )
-            .unwrap();
-            return Json(WebOutput { user });
+            let user: Option<User> = signup_and_update_db(query.email, query.password)
+                .await
+                .unwrap();
+
+            match user {
+                Some(_) => return Json(FailOrSucc::Success),
+                None => {
+                    return Json(FailOrSucc::Failure(String::from(
+                        "Error while trying to create your account",
+                    )));
+                }
+            }
+
+            //     auth::signup(
+            //     query.data["email"].to_string(),
+            //     query.data["password"].to_string(),
+            // )
+            // .await
+            // .unwrap_or_else(|| return None)
+            // .new_user()
+            // .await
+            // .expect("Error adding user")
         }
+        WebQuery::Login => {
+            return Json(FailOrSucc::Failure(String::from(
+                "Error logging in. Login is not POST request",
+            )));
+        }
+        WebQuery::UpdateBal(a) => match update_bal(query.email, query.password, a).await {
+            Some(_) => return Json(FailOrSucc::Success),
+            None => {
+                return Json(FailOrSucc::Failure(String::from(
+                    "Error while trying to update your account balance",
+                )));
+            }
+        },
+    }
+
+    // Ok(())
+}
+
+pub async fn handle_get_website(Json(query): Json<WebInput>) -> Json<WebOutput> {
+    match query.function {
+        WebQuery::Signup => {}
         _ => {}
     }
 
     Json(WebOutput {
-        user: User {
+        user: Some(User {
             email: String::new(),
             password: String::new(),
             apikey: String::new(),
             balance: 0,
-        },
+        }),
     })
 }
 
