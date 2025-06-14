b2d3a5273be8f6f1c8fe60b65ecf35efe76a0ec3
TeenCoder159 <no1umang@gmail.com>
2025-06-14 12:44:59 +0800

    Refactor Database to use 2 different databases

--- oneAI-backend/migrations/0001_userdata_table.sql
+++ oneAI-backend/migrations/0001_userdata_table.sql
@@ -1,10 +1,15 @@
 create table users (
-    email varchar not null,
+    id serial primary key,
+    email varchar not null unique,
     password varchar not null,
-    apikey varchar not null,
-    balance varchar not null
+    balance integer not null default 0
+);
+
+-- API KEYS TABLE
+create table api_keys (
+    id serial primary key,
+    user_id integer not null references users(id) on delete cascade,
+    key varchar not null unique
 );
 
 create unique index email on users (email);
-
-create unique index apikeys on users (apikey);
