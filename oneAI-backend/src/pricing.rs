b659b9bc931e4aabea3fec550f92ef141e85cb76
TeenCoder159 <no1umang@gmail.com>
2025-06-16 18:00:39 +0800

    Add various Models and modify a bit of the request code

--- oneAI-backend/src/pricing.rs
+++ oneAI-backend/src/pricing.rs
@@ -1,89 +1,94 @@
 #![allow(unused)]
 #![allow(non_camel_case_types)]
-use crate::*;
+use crate::{requests::AIProvider, *};
+
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
-pub enum OpenAIModel {
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum Model {
     Gpt4_1,
     Gpt4_1Mini,
     Gpt4_1Nano,
     GptO3,
     GptO4Mini,
+    ClaudeOpus4,
+    ClaudeSonnet4,
+    ClaudeHaiku3_5,
+    ClaudeOpus3,
+    ClaudeSonnet3_7,
+    ClaudeHaiku3,
+    DeepSeekR1,
+    DeepSeekV3,
+    Gemini25FlashPreview,
+    Gemini25ProPreview,
+    Gemini20Flash,
+    Gemini20FlashLite,
+    Gemini15Flash,
+    Gemini15Flash8B,
+    Gemini15Pro,
 }
 
-#[derive(Deserialize, Serialize)]
-pub enum ClaudeModel {
-    Opus4,
-    Sonnet4,
-    Haiku3_5,
-    Opus3,
-    Sonnet3_7,
-    Haiku3,
-}
-
-#[derive(Deserialize, Serialize)]
-pub enum GeminiModel {
-    Gem25_FlashPrev,
-    Gem25ProPrev,
-    Gem25FlashNativeAudio,
-}
-
-#[derive(Deserialize, Serialize)]
-pub enum MistralModel {}
-
-#[derive(Deserialize, Serialize)]
-pub enum DeepSeekModel {
-    Dsv3,
-    DsR1,
-}
-
-pub trait Model {
-    /// The prcing for the models per 1 million tokens
-    async fn price(&self) -> f32;
-}
-
-impl Model for OpenAIModel {
-    async fn price(&self) -> f32 {
+impl Model {
+    pub async fn price(&self) -> f32 {
         match self {
-            OpenAIModel::Gpt4_1 => return 11.0,
-            OpenAIModel::Gpt4_1Mini => return 2.5,
-            OpenAIModel::Gpt4_1Nano => return 0.6,
-            OpenAIModel::GptO3 => return 50.0,
-            OpenAIModel::GptO4Mini => return 6.0,
+            // ==== OpenAI ====
+            Model::Gpt4_1 => 10.4,
+            Model::Gpt4_1Mini => 2.08,
+            Model::Gpt4_1Nano => 0.52,
+            Model::GptO3 => 10.4,
+            Model::GptO4Mini => 5.72,
+
+            // ==== Anthropic ====
+            Model::ClaudeOpus4 => 93.6,
+            Model::ClaudeSonnet4 => 18.72,
+            Model::ClaudeHaiku3_5 => 4.992,
+            Model::ClaudeOpus3 => 93.6,
+            Model::ClaudeSonnet3_7 => 18.72,
+            Model::ClaudeHaiku3 => 1.82,
+
+            // ==== DeepSeek ====
+            Model::DeepSeekR1 => 1.4248,
+            Model::DeepSeekV3 => 2.4232,
+
+            // ==== Gemini (Google) ====
+            Model::Gemini25FlashPreview => 3.796,
+            Model::Gemini25ProPreview => 18.2,
+            Model::Gemini20Flash => 0.52,
+            Model::Gemini20FlashLite => 0.39,
+            Model::Gemini15Flash => 0.78,
+            Model::Gemini15Flash8B => 0.39,
+            Model::Gemini15Pro => 13.0,
         }
     }
-}
 
-impl Model for DeepSeekModel {
-    /// Returns $3.0 for R1 model (reasoner)
-    /// Returns $1.5 for V3 model (chat)
-    async fn price(&self) -> f32 {
+    pub async fn provider(&self) -> AIProvider {
         match self {
-            DeepSeekModel::DsR1 => return 3.0,
-            DeepSeekModel::Dsv3 => return 1.5,
-        }
-    }
-}
+            // ==== OpenAI ====
+            Model::Gpt4_1
+            | Model::Gpt4_1Mini
+            | Model::Gpt4_1Nano
+            | Model::GptO3
+            | Model::GptO4Mini => AIProvider::OpenAI,
 
-impl Model for ClaudeModel {
-    async fn price(&self) -> f32 {
-        match self {
-            ClaudeModel::Opus4 => return 91.0,
-            ClaudeModel::Sonnet4 => return 19.0,
-            ClaudeModel::Haiku3_5 => return 5.0,
-            ClaudeModel::Opus3 => return 91.0,
-            ClaudeModel::Sonnet3_7 => return 18.5,
-            ClaudeModel::Haiku3 => return 2.0,
-        }
-    }
-}
+            // ==== Anthropic ====
+            Model::ClaudeOpus4
+            | Model::ClaudeSonnet4
+            | Model::ClaudeHaiku3_5
+            | Model::ClaudeOpus3
+            | Model::ClaudeSonnet3_7
+            | Model::ClaudeHaiku3 => AIProvider::Anthropic,
 
-impl Model for GeminiModel {
-    async fn price(&self) -> f32 {
-        match self {
-            _ => {}
+            // ==== Gemini (Google) ====
+            Model::Gemini25FlashPreview
+            | Model::Gemini25ProPreview
+            | Model::Gemini20Flash
+            | Model::Gemini20FlashLite
+            | Model::Gemini15Flash
+            | Model::Gemini15Flash8B
+            | Model::Gemini15Pro => AIProvider::Gemini,
+
+            // ==== DeepSeek ====
+            Model::DeepSeekR1 | Model::DeepSeekV3 => AIProvider::DeepSeek,
         }
-        0.0
     }
 }
