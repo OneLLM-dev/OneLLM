303f2dd48870b4cfae32ece12e6753d5434e13ac
TeenCoder159 <no1umang@gmail.com>
2025-07-26 21:59:33 +0800

    Update tests and some other stupid errors

--- oneAI-backend/src/testing.rs
+++ oneAI-backend/src/testing.rs
@@ -31,17 +31,17 @@ mod tests {
         println!("User: {:#?}", unwrapped_hashed_user);
 
         unwrapped_hashed_user
-            .new_user()
+            .new_user(None)
             .await
             .expect("Error while trying to add new_user to database");
         assert_eq!(unwrapped_hashed_user.email, email);
         assert_eq!(unwrapped_hashed_user.balance, 0);
 
-        let res = login(email.clone(), password).await;
+        let res = login(None, email.clone(), password).await;
         println!("Res:\n{:#?}\n", res);
         assert_ne!(res, None);
 
-        User::delete_user(&email)
+        User::delete_user(None, &email)
             .await
             .expect("Error Deleting user: ");
     }
